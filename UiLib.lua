5194e1f6c2dcf1e7a6418c5068b372f5d68fab4a
pc <36272042+pcunplugged@users.noreply.github.com>
2021-11-18 18:01:53 -0700

    Update UiLib.lua

--- UiLib.lua
+++ UiLib.lua
@@ -3,6 +3,7 @@ local library = {}
 function library:CreateUI()
 	local background = Drawing.new("Square")
 	background.Size = Vector2.new(1,1)
+	background.Position = Vector2.new(0,0)
 	background.Filled = true
 end
 
